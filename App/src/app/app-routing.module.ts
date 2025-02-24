c9ab1f3cc771c2d437256e2bee5e9cf60879f588
pjovanovic12820rn <72506518+pjovanovic12820rn@users.noreply.github.com>
2025-02-24 20:27:41 +0100

    Revert "Implement Login functionality"
    This reverts commit 9f9c5ab1888a7e2ff710de85597b0c5633899d3d.

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  
-   {
-      path: 'login',
-      component: LoginComponent,
-  //canActivate: [LoginGuard],
-    },
+  // {
+  //   path: '',
+  //   component: LoginComponent,
+  //   canActivate: [LoginGuard],
+  // },
 
 ];
--- App/src/main.ts
+++ App/src/main.ts
@@ -1,13 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { LoginComponent } from './app/login/login.component';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
-
-  bootstrapApplication(LoginComponent, {
-    providers: [provideHttpClient(), provideRouter([])]
-  });
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
