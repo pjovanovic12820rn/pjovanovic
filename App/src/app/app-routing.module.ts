acff2106893ea09dc1f6d962dba8c1a5cba5a877
apantic3321rnRaf <apantic3321rn@raf.rs>
2025-02-24 23:21:26 +0100

    Add validation; Add Mock for Post request.

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import {RegisterEmployeeComponent} from './components/register-employee/register-employee.component';
 
 export const routes: Routes = [
   { path: 'employees', component: EmployeesComponent },
-  { path: 'employees/:id', component: EditEmployeeComponent }
+  { path: 'employees/:id', component: EditEmployeeComponent },
+  { path: 'register', component: RegisterEmployeeComponent }
 ];
