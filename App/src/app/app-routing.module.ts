acd50ca96c049eeda1c5e741497ac2e3f6ad24cf
unknown <kristinakatic100@gmail.com>
2025-02-25 23:38:06 +0100

    User service

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
-import {RegisterEmployeeComponent} from './components/register-employee/register-employee.component';
+import { RegisterEmployeeComponent } from './components/register-employee/register-employee.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 export const routes: Routes = [
+  { path: '', component: UsersListComponent },
   { path: 'employees', component: EmployeesComponent },
   { path: 'employees/:id', component: EditEmployeeComponent },
-  { path: 'register', component: RegisterEmployeeComponent }
+  { path: 'register', component: RegisterEmployeeComponent },
+  { path: '**', redirectTo: '' }
 ];
