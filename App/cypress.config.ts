598f3c21b1d60421d90da76391f61444e409d932
unknown <djveljkovic3019rn@raf.rs>
2025-04-03 22:34:43 +0200

    fix: fix ci

--- App/cypress.config.ts
+++ App/cypress.config.ts
@@ -5,13 +5,20 @@ export default defineConfig({
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/tests/e2e/**/*.spec.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/commands.ts',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
   component: {
     devServer: {
       framework: "angular",
       bundler: "webpack",
     },
-    
+
     specPattern: '**/*.cy.ts',
   },
 });
