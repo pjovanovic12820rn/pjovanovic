836cf4d2ccdeaf744042a59e4bb0e6f457bafe9f
unknown <djveljkovic3019rn@raf.rs>
2025-04-04 13:42:26 +0200

    test

--- App/cypress.config.ts
+++ App/cypress.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "mocha-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "spec, mocha-junit-reporter",
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "target/site/jacoco/results.xml",
+      toConsole: false
+    }
+  },
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/tests/e2e/**/*.spec.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/commands.ts',
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: false,
-      json: true
-    }
+    supportFile: 'cypress/support/commands.ts'
   },
   component: {
     devServer: {
