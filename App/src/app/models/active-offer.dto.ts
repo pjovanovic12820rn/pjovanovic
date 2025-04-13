1c06bc86eb76d5cc713287a6b2965cedc39d834d
mgajtanovic8521rn <mgajtanovic8521rn@raf.rs>
2025-04-14 00:37:27 +0200

    Uradjen active offers

---
+++ App/src/app/models/active-offer.dto.ts
@@ -0,0 +1,11 @@
+export interface ActiveOfferDto {
+    id: number;
+    stock: string;
+    amount: number;
+    pricePerStock: number;
+    settlementDate: string;
+    premium: number;
+    lastModified: string;
+    modifiedBy: string;
+  }
+  
